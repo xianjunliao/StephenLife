ad35766565b894c300446d551ce7e56e4a9d86ff
xianjun_liao <xianjun_liao@126.com>
2018-01-18 18:30:34 +0800

    wx011801

--- pages/website/index.js
+++ pages/website/index.js
@@ -1,44 +1,111 @@
 var msgs = '';
+var signature;
+var wxUserInfo = {};
 Page({
   data: {
-    focus: true
+
   },
   onLoad: function () {
+    var that = this;
+    
+
     wx.getUserInfo({
       success: function (res) {
-        var userInfo = res.userInfo
-        var nickName = userInfo.nickName
-        var avatarUrl = userInfo.avatarUrl
-        var gender = userInfo.gender
-        var province = userInfo.province
-        var city = userInfo.city
-        var country = userInfo.country
-        console.log(res);
-        var that = this
-        wx.request({
-          url: 'http://www.liaoxianjun.com/add',
-          data: {
-            code: nickName
-          },
-          header: {
-            'content-type': 'application/json' // 默认值
-          },
+        var user=res;
+        wx.getLocation({
+          type: 'wgs84',
           success: function (res) {
-            console.log(res.data)
-            if (res.data.code == "200") {
-         
-            }
-            if (res.data.code == "202") {
-          
-            }
-            else {
-     
-            }
-          }, fail: function (res) {
-
+            var location = res;
+            getUser(user, location);
           }
         })
       }
     })
   }
 })
+function getUser(user,location) {
+  wx.request({
+    url: 'http://www.liaoxianjun.com/wx/user/getUserInfo',
+    data: {
+      signature: user.signature
+    },
+    header: {
+      'content-type': 'application/json' // 默认值
+    },
+    success: function (res) {
+      if (res.data.code == "200") {
+        wx.getSystemInfo({
+          success: function (res) {
+            saveLogin(user, res,location);
+          }
+        })
+      } else if (res.data.code == "404") {
+        saveUser(user);
+        wx.getSystemInfo({
+          success: function (res) {
+            saveLogin(user, res, location);
+          }
+        })
+      } else {
+
+      }
+    }
+  })
+}
+function saveLogin(user, system, location) {
+  var userInfo = user.userInfo;
+  wx.request({
+    url: 'http://www.liaoxianjun.com/wx/login/save',
+    data: {
+      signature: user.signature,
+      nickname: userInfo.nickName,
+      avatarurl: userInfo.avatarUrl,
+      loginaddress: location.latitude +','+location.longitude,
+      brand: system.brand,
+      model: system.model,
+      version: system.version,
+      language: system.language,
+      system: system.system
+    },
+    header: {
+      'content-type': 'application/json' // 默认值
+    },
+    success: function (res) {
+      console.log(res.data)
+    }, fail: function (res) {
+
+    }
+  })
+}
+function saveUser(res) {
+  var userInfo = res.userInfo;
+  wx.request({
+    url: 'http://www.liaoxianjun.com/wx/user/save',
+    data: {
+      signature: res.signature,
+      nickname: userInfo.nickName,
+      avatarurl: userInfo.avatarUrl,
+      gender: userInfo.gender,
+      province: userInfo.province,
+      city: userInfo.city,
+      country: userInfo.country
+    },
+    header: {
+      'content-type': 'application/json' // 默认值
+    },
+    success: function (res) {
+      console.log(res.data)
+      if (res.data.code == "200") {
+
+      }
+      if (res.data.code == "202") {
+
+      }
+      else {
+
+      }
+    }, fail: function (res) {
+
+    }
+  })
+}
